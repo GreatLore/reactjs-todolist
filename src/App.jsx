74bca6ebeda06a1c1d39f627fba958200dc4f7d3
LonnieGonzales <glonnie0027@gmail.com>
2024-09-06 09:42:42 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react"
+import TodoInput from "./components/ToDoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+
+  const [todos, setTodos] = useState([
+  ]);
+  const [todoValue, setTodoValue] = useState('');
+
+  function persistData(newList){
+    localStorage.setItem('todos', JSON.stringify({
+      todos:newList
+    }))
+  }
+  function handleAddTodos(newTodo){
+    const newTodos = [...todos, newTodo];
+    persistData(newTodos)
+    setTodos(newTodos);
+  } 
+  function handleDeleteTodo(index){
+    const newTodos = todos.filter((todo, indx)=>{
+      return indx !==index
+    });
+    persistData(newTodos)
+    setTodos(newTodos);
+  } 
+  function handleEditTodo(index){
+    const ValueToEdit = todos[index];
+    setTodoValue(ValueToEdit);
+    handleDeleteTodo(index)
+  } 
+
+  useEffect(()=>{
+    if(!localStorage){
+      return
+    }
+    
+    let localTodos = localStorage.getItem('todos')
+    if(!localTodos) {
+      return
+    }
+    
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+    
+  },[])
+  
+  return (
+    <>
+      <TodoInput 
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        handleAddTodos={handleAddTodos}/>
+      <TodoList 
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+        todos={todos}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+const {children, handleDeleteTodo, index, handleEditTodo} =props;
+  return (
+    <div>
+        <li className='todoItem'>
+            {children}
+            <div className='actionsContainer'>
+                <button onClick={()=>{
+                    handleEditTodo(index)
+                }}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={()=>{
+                    handleDeleteTodo(index)
+                }}>
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        </li>   
+    </div>
+  )
+}
+
+
+                    // <li key={indx} className='todoItem'>
+                    //     {todo}
+                    //     <i class="fa-solid fa-pen-to-square"></i>
+                    // </li>   
